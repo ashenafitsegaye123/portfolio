ec44284f166c7c61ed122ea9047375ad159eea33
Ashenafi Tsegaye <ashenafitsegish@gmail.com>
2025-06-26 21:19:32 +0300

    first commit

---
+++ components/experience/Experience.jsx
@@ -0,0 +1,149 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import './experience.css';
+
+const Experience = () => {
+    const[toggleState, setToggleState] = useState(false);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+  return (
+    <div className="section experience section" id="experience">
+        <h2 className="section__title">Experience 💼</h2>
+        <span className="section__subtitle">What I work as</span>
+
+        <div className="exp__container container grid">
+            <div className="exp__content">
+                <div>
+                    <i className="uil uil-web-grid exp__icon"></i>
+                    <h3 className="exp__title">Software <br /> Engineer </h3>
+                </div>
+
+                <div>
+                    <span className="exp__comp">Edatis KPG</span>
+                </div>
+
+                <span className="exp__button" onClick={() => toggleTab(1)}>View More <i className="uil uil-arrow-right exp__button-icon"></i></span>
+
+                <div className={toggleState === 1 ? "exp__modal active-modal" : "exp__modal"}>
+                    <div className="exp__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times exp__modal-close"></i>
+
+                        <h3 className="exp__modal-title">Software Engineer <br /> Dec'23 - Currently Working 🧑🏻‍💻</h3>
+                        <p className="exp__modal-description">Working currently as a Software Engineer for full-time at Edatis KPG.
+                            Collaborating with an international team for the design and development of
+                            web applications.
+                        </p>
+
+                        <ul className="exp__modal-experiences grid">
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Web Page Development.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">I create Frontend UI Interfaces With Angular.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">I create Backend API Interactions With Symfony.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="exp__content">
+                <div>
+                    <i className="uil uil-arrow exp__icon"></i>
+                    <h3 className="exp__title">Software <br /> Developer</h3>
+                </div>
+
+                <div>
+                    <span className="exp__comp">Neocortex</span>
+                </div>
+
+                <span className="exp__button" onClick={() => toggleTab(2)}>View More <i className="uil uil-arrow-right exp__button-icon"></i></span>
+
+                <div className={toggleState === 2 ? "exp__modal active-modal" : "exp__modal"}>
+                    <div className="exp__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times exp__modal-close"></i>
+
+                        <h3 className="exp__modal-title">Software Developer Intern <br /> Feb'23 - Sep'23</h3>
+                        <p className="exp__modal-description">Worked at Neocortex as Intern for 8 months.
+                            Providing quality work to clients and company.
+                        </p>
+
+                        <ul className="exp__modal-experiences grid">
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">I developed the User Interfaces.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Web Page Development.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Contributed to improving employee satisfaction.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="exp__content">
+                <div>
+                    <i className="uil uil-arrow exp__icon"></i>
+                    <h3 className="exp__title">Software <br /> Developer</h3>
+                </div>
+
+                <div>
+                    <span className="exp__comp">Saheelha</span>
+                </div>
+
+                <span className="exp__button" onClick={() => toggleTab(3)}>View More <i className="uil uil-arrow-right exp__button-icon"></i></span>
+
+                <div className={toggleState === 3 ? "exp__modal active-modal" : "exp__modal"}>
+                    <div className="exp__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times exp__modal-close"></i>
+
+                        <h3 className="exp__modal-title">Software Developer Remote <br /> Sep'20 - Feb'22</h3>
+                        <p className="exp__modal-description">Worked at Saheelha as Software mobile developer for 18 months.
+                            Providing quality work to clients and company.
+                        </p>
+
+                        <ul className="exp__modal-experiences grid">
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">I developed the User Mobile Interfaces.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Mobile App Development.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Backend Intercation API Development.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
---
+++ components/home/Social.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/jawher_kallel/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://in.linkedin.com/in/jawher-kallel" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/JawherKl" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
